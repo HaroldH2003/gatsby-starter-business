c227783b4a7de3b4edd57e23ad1b7d68e59132b3
Manh Huy Hoang <mhuy2003@gmail.com>
2020-01-03 16:12:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  siteTitle: 'LetSnack', // Site title.
-  siteTitleAlt: 'LetSnack', // Alternative site title for SEO.
+  siteTitle: 'Harold Talks Tech', // Site title.
+  siteTitleAlt: 'Harold Talks Tech', // Alternative site title for SEO.
   siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
   siteUrl: 'https://loving-tesla-91755d.netlify.com/', // Domain of your website without pathPrefix.
   pathPrefix: '', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
@@ -8,12 +8,12 @@ module.exports = {
   siteRss: '/rss.xml',
   siteFBAppID: '', // FB Application ID for using app insights
   googleTagManagerID: '', // GTM tracking ID.
-  disqusShortname: 'gatsby-business-starter', // Disqus shortname.
-  userName: 'Vaibhav Sharma',
+  disqusShortname: 'harold-talks-tech', // Disqus shortname.
+  userName: 'Harold Hoang',
   userTwitter: 'vaibhaved',
   userLocation: 'Delhi NCR, India',
   userDescription: '',
-  copyright: 'Copyright © LetSnack 2019. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
+  copyright: 'Copyright © HaroldTalksTech 2019. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
   themeColor: '#00d1b2', // Used for setting manifest and progress theme colors.
   backgroundColor: '#ffffff', // Used for setting manifest background color.
 }
